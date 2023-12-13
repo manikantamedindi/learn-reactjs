24fedf8b55e47adcf1859e84a3474b20535feef7
Manikanta M <manikanta.m@labsquire.com>
2023-12-13 20:20:20 +0530

    Updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,60 @@
-const FoodBlock = () => {
+import React from "react";
+
+const FoodItems = [
+  {
+    name: "Silver Dum Biryani",
+    recepie: "Biryani, Chinese, North Indian, Seafood Gurunanak.",
+    rating: "4.2 Star Rating",
+    imageUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p2k9r9zkeqcjhohsgrfu",
+  },
+  {
+    name: "Golden Dum Biryani",
+    recepie: "Biryani, Chinese, North Indian, Seafood Gurunanak.",
+    rating: "4.5 Star Rating",
+    imageUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p2k9r9zkeqcjhohsgrfu",
+  },
+  {
+    name: "Silver Dum Biryani",
+    recepie: "Biryani, Chinese, North Indian, Seafood Gurunanak.",
+    rating: "4.2 Star Rating",
+    imageUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p2k9r9zkeqcjhohsgrfu",
+  },
+  {
+    name: "Golden Dum Biryani",
+    recepie: "Biryani, Chinese, North Indian, Seafood Gurunanak.",
+    rating: "4.5 Star Rating",
+    imageUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p2k9r9zkeqcjhohsgrfu",
+  },
+  {
+    name: "Silver Dum Biryani",
+    recepie: "Biryani, Chinese, North Indian, Seafood Gurunanak.",
+    rating: "4.2 Star Rating",
+    imageUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p2k9r9zkeqcjhohsgrfu",
+  },
+  {
+    name: "Golden Dum Biryani",
+    recepie: "Biryani, Chinese, North Indian, Seafood Gurunanak.",
+    rating: "4.5 Star Rating",
+    imageUrl:
+      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p2k9r9zkeqcjhohsgrfu",
+  },
+];
+
+const FoodBlock = ({ name, recepie, rating, imageUrl }) => {
   return (
     <div className="bg-white rounded-md shadow-md overflow-hidden res-card transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer">
       <div className="w-full h-48 overflow-hidden image">
-        <img
-          className="w-full h-full object-cover"
-          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/p2k9r9zkeqcjhohsgrfu"
-          alt=""
-        />
+        <img className="w-full h-full object-cover" src={imageUrl} alt={name} />
       </div>
       <div className="p-4 content">
-        <h3 className="text-xl font-bold mb-2">Silver Dum Biryani</h3>
-        <h4 className="text-sm text-gray-700 mb-2">
-          Biryani, Chinese, North Indian, Seafood Gurunanak.
-        </h4>
-        <h4 className="text-yellow-500 font-semibold">4.2 Star Rating</h4>
+        <h3 className="text-xl font-bold mb-2">{name}</h3>
+        <h4 className="text-sm text-gray-700 mb-2">{recepie}</h4>
+        <h4 className="text-yellow-500 font-semibold">{rating}</h4>
       </div>
     </div>
   );
@@ -24,19 +65,10 @@ const Homepage = () => {
     <div className="HomePage">
       <section>
         <div className="container mx-auto pt-10 pb-10">
-          <div class="flex flex-wrap justify-center gap-7">
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
-            <FoodBlock />
+          <div className="flex flex-wrap justify-center gap-7">
+            {FoodItems.map((food, index) => (
+              <FoodBlock key={index} {...food} />
+            ))}
           </div>
         </div>
       </section>
