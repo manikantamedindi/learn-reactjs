695b6921b984270cd3b2a8b9c89eb711dbd9dcda
Manikanta M <manikanta.m@labsquire.com>
2024-05-08 16:45:40 +0530

    Functional component learning

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", className: "heading" },
-  "Hello world from React"
+// Arrow Function
+const HeadingComponent = () => (
+  <div className="container">
+    <h1 className="heading">Heading Component</h1>
+    <Title />
+    <Title2 />
+  </div>
 );
+
+// Normal Function
+function Title2() {
+  return <h3>Inside Another Title</h3>;
+}
+
+const Title = () => <h2>This is Inside Title Component</h2>;
+
 const root = createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
